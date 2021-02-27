0790be9db20e89b1461702f7d8ec1de2400f6f08
DV G-Meaning <dev@globalmeaning.com>
2021-02-27 08:57:35 -0300

    [DI-4] junit financialSummary scrapping

--- src/test/java/ar/com/pa/service/ScrappingFormatTest.java
+++ src/test/java/ar/com/pa/service/ScrappingFormatTest.java
@@ -1,6 +1,5 @@
 package ar.com.pa.service;
 
-
 import java.io.IOException;
 import java.sql.Date;
 import java.util.Collection;
@@ -21,89 +20,72 @@ import ar.com.pa.utils.MapperUtils;
 import ar.com.pa.utils.PatternResource;
 import ar.com.pa.utils.ValidateUtils;
 
-
 public class ScrappingFormatTest {
-	
+
 	@Mock
 	public static Elements elements;
-	
+
 	@Mock
 	public static Elements quarterPeriodDates;
 
-	private ScrappingFormat scrappingMock = new ScrappingFormat(); 
-	
+	private ScrappingFormat scrappingMock = new ScrappingFormat();
 
 	@Test
 	@BeforeAll
 	public static void getConnection() {
-		
+
 		Document doc;
 		try {
 			doc = Jsoup.connect(ScrappingFormatConstant.httpUrl).get();
 			setElements(doc.getElementsByTag("td"));
 			setQuarterPeriodDates(doc.getElementsByTag("th"));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	
-
-	
 	@Test
 	void getValidElementsTest() {
-		
+
 		List<String> validElements = scrappingMock.getValidElements(getElements());
-		
+
 		List<String> invalidElements = Lists.newArrayList(ScrappingFormatConstant.mockElements);
-					
-		List<String> mockList = Stream.of(validElements, invalidElements)
-				                .flatMap(Collection::stream)
-				                .filter(ValidateUtils::isSummaryModelValue)
-				                .collect(Collectors.toList());
-				
-		 Assertions.assertEquals( mockList, validElements); 		
+
+		List<String> mockList = Stream.of(validElements, invalidElements).flatMap(Collection::stream)
+				.filter(ValidateUtils::isSummaryModelValue).collect(Collectors.toList());
+
+		Assertions.assertEquals(mockList, validElements);
 	}
-	
-	
-	
+
 	@Test
 	void getQuarterPeriodListTest() {
-			
-			List<String> mockedList = Lists.newArrayList(ScrappingFormatConstant.mockPeriods);  
-			
-		ImmutableList<Date> mockQuarterPeriodList = ImmutableList.copyOf(mockedList.stream()
-																.distinct()
-																.filter(PatternResource::dateStringPattern)
-																.map(MapperUtils::toDate)
-																.collect(Collectors.toList()));
 
+		List<String> mockedList = Lists.newArrayList(ScrappingFormatConstant.mockPeriods);
 
-			 List<Date> quarterPeriodDates = scrappingMock.getQuarterPeriodDate(getQuarterPeriodDates());
-		
-			 Assertions.assertEquals( quarterPeriodDates, mockQuarterPeriodList); 
+		ImmutableList<Date> mockQuarterPeriodList = ImmutableList.copyOf(mockedList.stream().distinct()
+				.filter(PatternResource::dateStringPattern).map(MapperUtils::toDate).collect(Collectors.toList()));
+
+		List<Date> quarterPeriodDates = scrappingMock.getQuarterPeriodDate(getQuarterPeriodDates());
+
+		Assertions.assertEquals(quarterPeriodDates, mockQuarterPeriodList);
 	}
-
 
 	public static void setElements(Elements elements) {
 		ScrappingFormatTest.elements = elements;
 	}
 
-
 	public static void setQuarterPeriodDates(Elements quarterPeriodDates) {
 		ScrappingFormatTest.quarterPeriodDates = quarterPeriodDates;
 	}
 
-
 	public static Elements getElements() {
 		return elements;
 	}
+
 	public static Elements getQuarterPeriodDates() {
 		return quarterPeriodDates;
 	}
 
-
-	
 }
